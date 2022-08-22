1eb27f2863ae3f6f4991621a03593c8341b0dd45
Landon Cayia <landon.cayia@gmail.com>
2022-08-22 12:14:06 -0400

    Add map to view

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -5,12 +5,22 @@
 //  Created by Landon Cayia on 8/20/22.
 //
 
+import MapKit
 import SwiftUI
 
 struct ContentView: View {
+    @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 50, longitude: 0), span: MKCoordinateSpan(latitudeDelta: 25, longitudeDelta: 25))
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Map(coordinateRegion: $mapRegion)
+                .ignoresSafeArea()
+            
+            Circle()
+                .fill(.blue)
+                .opacity(0.3)
+                .frame(width: 32, height: 32)
+        }
     }
 }
 
