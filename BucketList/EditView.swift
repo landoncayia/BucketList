96e2e35e93d90ffdc5b10b7600b0afb41ac6de9c
Landon Cayia <landon.cayia@gmail.com>
2022-08-22 12:51:38 -0400

    Make edit view pass new location back

--- BucketList/EditView.swift
+++ BucketList/EditView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct EditView: View {
     @Environment(\.dismiss) var dismiss
     var location: Location
+    var onSave: (Location) -> Void
     
     @State private var name: String
     @State private var description: String
@@ -25,14 +26,22 @@ struct EditView: View {
             .navigationTitle("Place details")
             .toolbar {
                 Button("Save") {
+                    var newLocation = location
+                    newLocation.name = name
+                    newLocation.description = description
+                    
+                    // Pass the new location back to whatever called this view
+                    onSave(newLocation)
                     dismiss()
                 }
             }
         }
     }
     
-    init(location: Location) {
+    // @escaping tells Swift to keep the memory alive so that it can be called later on (when save is tapped)
+    init(location: Location, onSave: @escaping (Location) -> Void) {
         self.location = location
+        self.onSave = onSave
         
         _name = State(initialValue: location.name)
         _description = State(initialValue: location.description)
@@ -41,6 +50,6 @@ struct EditView: View {
 
 struct EditView_Previews: PreviewProvider {
     static var previews: some View {
-        EditView(location: Location.example)
+        EditView(location: Location.example) { _ in }
     }
 }
