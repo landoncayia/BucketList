7e9e00033121aee5305c921a9e4c44ad0f1b3ef3
Landon Cayia <landon.cayia@gmail.com>
2022-08-22 12:21:38 -0400

    Make add location button functional

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -10,10 +10,13 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 50, longitude: 0), span: MKCoordinateSpan(latitudeDelta: 25, longitudeDelta: 25))
+    @State private var locations = [Location]()
     
     var body: some View {
         ZStack {
-            Map(coordinateRegion: $mapRegion)
+            Map(coordinateRegion: $mapRegion, annotationItems: locations) { location in
+                MapMarker(coordinate: CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude))
+            }
                 .ignoresSafeArea()
             
             Circle()
@@ -28,7 +31,8 @@ struct ContentView: View {
                     Spacer()
                     
                     Button {
-                        // create a new location
+                        let newLocation = Location(id: UUID(), name: "New location", description: "", latitude: mapRegion.center.latitude, longitude: mapRegion.center.longitude)
+                        locations.append(newLocation)
                     } label: {
                         Image(systemName: "plus")
                     }
