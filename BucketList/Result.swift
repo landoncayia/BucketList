3decb082bd24e88d3971f8957191d32d9de5eb1f
Landon Cayia <landon.cayia@gmail.com>
2022-08-23 12:34:48 -0400

    Add comments to Result, Query, and Page structs

--- BucketList/Result.swift
+++ BucketList/Result.swift
@@ -7,14 +7,17 @@
 
 import Foundation
 
+/// A container that holds the result of a query to the Wikipedia API for places nearby to given GPS coordinates.
 struct Result: Codable {
     let query: Query
 }
 
+/// A container that holds the pages, as a dictionary, returned within a query to the Wikipedia API for places nearby to given GPS coordinates.
 struct Query: Codable {
     let pages: [Int: Page]
 }
 
+/// Represents a page returned via a query to the Wikipedia API for places nearby to given GPS coordinates.
 struct Page: Codable {
     let pageid: Int
     let title: String
