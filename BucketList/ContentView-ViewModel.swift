d641ba88ded9ad324d225422d471a938117359b5
Landon Cayia <landon.cayia@gmail.com>
2022-08-24 11:01:10 -0400

    Enable reading and saving app data
    This is done using the documents directory for the app, and is backed
    by strong encryption (only accessible if the person's phone is
    unlocked).

--- BucketList/ContentView-ViewModel.swift
+++ BucketList/ContentView-ViewModel.swift
@@ -13,9 +13,28 @@ extension ContentView {
     @MainActor class ViewModel: ObservableObject {
         @Published var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 50, longitude: 0), span: MKCoordinateSpan(latitudeDelta: 25, longitudeDelta: 25))
         @Published private(set) var locations = [Location]()
-        
         // Optional because a location may or may not be selected
         @Published var selectedPlace: Location?
+        
+        let savePath = FileManager.documentsDirectory.appendingPathComponent("SavedPlaces")
+        
+        init() {
+            do {
+                let data = try Data(contentsOf: savePath)
+                locations = try JSONDecoder().decode([Location].self, from: data)
+            } catch {
+                locations = []
+            }
+        }
+        
+        func save() {
+            do {
+                let data = try JSONEncoder().encode(locations)
+                try data.write(to: savePath, options: [.atomicWrite, .completeFileProtection])
+            } catch {
+                print("Unable to save data.")
+            }
+        }
         
         func addLocation() {
             let newLocation = Location(id: UUID(), name: "New location", description: "", latitude: mapRegion.center.latitude, longitude: mapRegion.center.longitude)
