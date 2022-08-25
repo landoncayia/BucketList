3cb196934e6facb1ed0e5a4dbed8aa1be1fa361d
Landon Cayia <landon.cayia@gmail.com>
2022-08-25 16:50:24 -0400

    Convert EditView to MVVM architecture

--- BucketList/EditView.swift
+++ BucketList/EditView.swift
@@ -8,35 +8,26 @@
 import SwiftUI
 
 struct EditView: View {
-    /// Tracks the state of a network fetch request from the Wikipedia API
-    enum LoadingState {
-        case loading, loaded, failed
-    }
-    
     @Environment(\.dismiss) var dismiss
+    @StateObject private var viewModel: ViewModel
+    
     var location: Location
     var onSave: (Location) -> Void
-    
-    @State private var name: String
-    @State private var description: String
-    
-    @State private var loadingState = LoadingState.loading
-    @State private var pages = [Page]()
     
     var body: some View {
         NavigationView {
             Form {
                 Section {
-                    TextField("Place name", text: $name)
-                    TextField("Description", text: $description)
+                    TextField("Place name", text: $viewModel.name)
+                    TextField("Description", text: $viewModel.description)
                 }
                 
                 Section("Nearby…") {
-                    switch loadingState {
+                    switch viewModel.loadingState {
                     case .loading:
                         Text("Loading…")
                     case .loaded:
-                        ForEach(pages, id: \.pageid) { page in
+                        ForEach(viewModel.pages, id: \.pageid) { page in
                             Text(page.title)
                                 .font(.headline)
                             + Text("; ")
@@ -50,19 +41,14 @@ struct EditView: View {
             }
             .navigationTitle("Place details")
             .toolbar {
-                Button("Save") {
-                    var newLocation = location
-                    newLocation.id = UUID()
-                    newLocation.name = name
-                    newLocation.description = description
-                    
+                Button("Save") {                   
                     // Pass the new location back to whatever called this view
-                    onSave(newLocation)
+                    onSave(viewModel.newLocation)
                     dismiss()
                 }
             }
             .task {
-                await fetchNearbyPlaces()
+                await viewModel.fetchNearbyPlaces()
             }
         }
     }
@@ -72,28 +58,7 @@ struct EditView: View {
         self.location = location
         self.onSave = onSave
         
-        _name = State(initialValue: location.name)
-        _description = State(initialValue: location.description)
-    }
-    
-    /// An asynchronous function that performs an API fetch request to Wikipedia for nearby places.
-    func fetchNearbyPlaces() async {
-        let urlString = "https://en.wikipedia.org/w/api.php?ggscoord=\(location.coordinate.latitude)%7C\(location.coordinate.longitude)&action=query&prop=coordinates%7Cpageimages%7Cpageterms&colimit=50&piprop=thumbnail&pithumbsize=500&pilimit=50&wbptterms=description&generator=geosearch&ggsradius=10000&ggslimit=50&format=json"
-        
-        // Ensure that the URL is working
-        guard let url = URL(string: urlString) else {
-            print("Bad URL: \(urlString)")
-            return
-        }
-        
-        do {
-            let (data, _) = try await URLSession.shared.data(from: url)
-            let items = try JSONDecoder().decode(Result.self, from: data)
-            pages = items.query.pages.values.sorted()
-            loadingState = .loaded
-        } catch {
-            loadingState = .failed
-        }
+        _viewModel = StateObject(wrappedValue: ViewModel(location: location))
     }
 }
 
